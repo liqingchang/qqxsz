36e440201b54a169b5388797d7bab9c5e7a4e0af
terry <chang2qing@gmail.com>
2017-08-06 16:41:34 +0800

    initial commit

---
+++ utils/util.js
@@ -0,0 +1,32 @@
+function formatTime(date) {
+  var year = date.getFullYear()
+  var month = date.getMonth() + 1
+  var day = date.getDate()
+
+  var hour = date.getHours()
+  var minute = date.getMinutes()
+  var second = date.getSeconds()
+
+
+  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+}
+
+function formatNumber(n) {
+  n = n.toString()
+  return n[1] ? n : '0' + n
+}
+
+/**
+ * 显示内容
+ */
+function toast(msg){
+  wx.showToast({
+    title: msg,
+    duration: 2000
+  })
+}
+
+module.exports = {
+  formatTime: formatTime,
+  toast: toast
+}
