6e86cafe030cc44adc017fba0febc5ce9b2cbe21
terry <chang2qing@gmail.com>
2017-08-12 17:20:12 +0800

    增加生成成功后跳转页面

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,27 +5,27 @@ var util = require('../../utils/util.js')
 var tmpFilePath
 var uploadTask
 
-
 Page({
   data: {
-     button_text: '生成学生证',
-     name : "老王",
-     colleges: "家里系",
-     sexIndex: 0,
-     sexList: ["男", "女", "妖"],
-     degreeIndex: 0,
-     degreeList:["本科生", "硕士生", "博士生"],
-     id_number: "2017000001",
-     date: "2017",
-  }, bindNameInput: function(e) {
+    button_text: '生成学生证',
+    name: "老王",
+    colleges: "家里系",
+    sexIndex: 0,
+    sexList: ["男", "女", "妖"],
+    degreeIndex: 0,
+    degreeList: ["本科生", "硕士生", "博士生"],
+    id_number: "2017000001",
+    date: "2017",
+    uploading: false
+  }, bindNameInput: function (e) {
     this.setData({
-      name: e.detail.value,      
+      name: e.detail.value,
     })
   }, bindIdInput: function (e) {
     this.setData({
       id_number: e.detail.value,
     })
-  }, bindCollegesInput: function(e) {
+  }, bindCollegesInput: function (e) {
     this.setData({
       colleges: e.detail.value,
     })
@@ -41,8 +41,9 @@ Page({
     this.setData({
       date: e.detail.value
     })
-  },  upload_pic: function(e) {
+  }, upload_pic: function (e) {
     var that = this
+
     wx: wx.chooseImage({
       count: 1, // 默认9
       sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
@@ -50,6 +51,9 @@ Page({
       success: function (res) {
         // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
         tmpFilePath = res.tempFilePaths
+        that.setData({
+          uploading: true
+        })
         uploadTask = wx.uploadFile({
           url: 'https://4gun.net/upload',
           filePath: res.tempFilePaths[0],
@@ -64,21 +68,24 @@ Page({
           },
           success: function (res) {
             console.log('返回值:', res.data)
+            wx.navigateTo({
+              url: '../xsz/xsz?image=' + res.data
+            })
             that.setData({
-              image_src: res.data
+              // image_src: res.data,
+              uploading: false
             })
           }
         })
         uploadTask.onProgressUpdate((res) => {
           console.log('上传进度', res.progress)
-          console.log('已经上传的数据长度', res.totalBytesSent)
-          console.log('预期需要上传的数据总长度', res.totalBytesExpectedToSend)
+
         })
       }
     })
 
     uploadTask
-   
+
     //  ,
     // this.setData({
     //   image_src: 'http://4gun.net/images/szj.jpg'
